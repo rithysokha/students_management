8e064bb5436c7c9d5fb5d86c5bf59ea2bb13558f
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-10-21 08:49:38 +0700

    test folder

---
+++ src/test/java/com/student/student_management/controller/FacultyControllerTest.java
@@ -0,0 +1,36 @@
+package com.student.student_management.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacultyControllerTest {
+
+    @Test
+    void getAllFaculty() {
+    }
+
+    @Test
+    void getOneFacultyByID() {
+    }
+
+    @Test
+    void deleteFacultyByID() {
+    }
+
+    @Test
+    void deleteMultipleFaculty() {
+    }
+
+    @Test
+    void createFaculty() {
+    }
+
+    @Test
+    void testCreateFaculty() {
+    }
+
+    @Test
+    void updateFacultyById() {
+    }
+}
