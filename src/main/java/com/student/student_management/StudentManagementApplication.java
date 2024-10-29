f700bff626e1e876d33e88584fc90916ebb712f5
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-10-29 16:03:49 +0700

    change time zone

--- src/main/java/com/student/student_management/StudentManagementApplication.java
+++ src/main/java/com/student/student_management/StudentManagementApplication.java
@@ -1,9 +1,12 @@
 package com.student.student_management;
 
 import com.student.student_management.config.PropertiesConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableConfigurationProperties(PropertiesConfig.class)
@@ -12,5 +15,8 @@ public class StudentManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
     }
-
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Phnom_Penh"));
+    }
 }
